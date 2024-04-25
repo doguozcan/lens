f453a173ce5d1dab8d2e5eaadfd0277742258993
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-04-25 13:28:28 +0300

    Initialize setting up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+
 const App = () => {
-  return <p>lens</p>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  )
 }
 export default App
