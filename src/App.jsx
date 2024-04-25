df80a8040200f80f188859510e6162c4fef193ac
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-04-25 13:48:40 +0300

    Add auth providers and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
 import { supabase } from "../api/supabaseClient"
+import { useAuth } from "../hooks/useAuth"
+import { Navigate } from "react-router-dom"
 
 const Login = () => {
+  const { session } = useAuth()
   const [email, setEmail] = useState("")
   const [loading, setLoading] = useState(false)
 
@@ -18,13 +21,16 @@ const Login = () => {
     setLoading(false)
   }
 
-  return (
+  return session ? (
+    <Navigate to="/" />
+  ) : (
     <div className="flex h-screen justify-center items-center">
       <div className="flex flex-col gap-2 w-full max-w-sm">
         <p className="text-primary text-5xl animate-pulse">lens</p>
         <p>welcome to lens</p>
         <p>enter your email to get a magic link</p>
         <input
+          type="email"
           className="input input-primary input-bordered input-sm"
           disabled={loading}
           value={email}
