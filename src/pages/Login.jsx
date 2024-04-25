c5082ad1932e65ccfb833731489e14116d083224
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-04-25 09:28:50 +0300

    Implement login functionality through OTP
    Use Supabase's authentication system in order to get OTP (one-time password).

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react"
+import { supabase } from "../api/supabaseClient"
+
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleSendMagicLink = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    const { error } = await supabase.auth.signInWithOtp({ email })
+    if (error) {
+      alert(error.error_description || error.message)
+    } else {
+      alert("check your mailbox")
+      setEmail("")
+    }
+    setLoading(false)
+  }
+
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="flex flex-col gap-2 w-full max-w-sm">
         <p className="text-primary text-5xl animate-pulse">lens</p>
         <p>welcome to lens</p>
         <p>enter your email to get a magic link</p>
-        <input className="input input-primary input-bordered input-sm" />
-        <button className="btn btn-primary btn-outline btn-sm">send</button>
+        <input
+          className="input input-primary input-bordered input-sm"
+          disabled={loading}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          className="btn btn-primary btn-outline btn-sm"
+          onClick={handleSendMagicLink}
+          disabled={loading}
+        >
+          send
+        </button>
       </div>
     </div>
   )
