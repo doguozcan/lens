5bf0e86879e767969baf2c9dc4b5a0237aff04c9
Doğu Özcan <76822280+doguozcan@users.noreply.github.com>
2024-04-26 09:01:48 +0300

    Implement delete image functionality

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -6,33 +6,56 @@ const Images = () => {
   const { user } = useAuth()
   const [imageNames, setImagesNames] = useState([])
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      const { data, error } = await supabase.storage
-        .from("images")
-        .list(`${user.id}`)
+  const fetchImages = async () => {
+    const { data, error } = await supabase.storage
+      .from("images")
+      .list(`${user.id}`)
 
-      if (error) {
-        console.log(error)
-      }
-
-      if (data) {
-        setImagesNames(data.map((item) => item.name))
-      }
+    if (error) {
+      console.log(error)
     }
 
+    if (data) {
+      setImagesNames(data.map((item) => item.name))
+    }
+  }
+
+  useEffect(() => {
     fetchImages()
   }, [])
+
+  const deleteImage = async (imageFolder, imageName) => {
+    const { data, error } = await supabase.storage
+      .from("images")
+      .remove([`${imageFolder}/${imageName}`])
+
+    if (data) {
+      fetchImages()
+      console.log(data)
+    }
+
+    if (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 my-5">
       {imageNames.map((imageName) => (
-        <img
-          className="h-96 w-full rounded-xl object-cover"
-          src={`${
-            import.meta.env.VITE_SUPABASE_URL
-          }/storage/v1/object/public/images/${user.id}/${imageName}`}
-        />
+        <div key={imageName} className="relative">
+          <img
+            className="h-96 w-full rounded-xl object-cover"
+            src={`${
+              import.meta.env.VITE_SUPABASE_URL
+            }/storage/v1/object/public/images/${user.id}/${imageName}`}
+          />
+          <button
+            className="btn btn-error absolute top-1 right-1 text-white"
+            onClick={() => deleteImage(user.id, imageName)}
+          >
+            delete
+          </button>
+        </div>
       ))}
     </div>
   )
